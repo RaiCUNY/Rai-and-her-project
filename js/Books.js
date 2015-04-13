168219c1bc617d7b3e1e3c540822e81e76c427c6
Ayan Lahiri <ayanlahiri@Ayans-MacBook-Pro.local>
2015-04-13 19:35:30 -0400

    carosuel stuff

---
+++ js/Books.js
@@ -0,0 +1,15 @@
+"books":[
+    {
+     "title":"Night of the Gun"
+     "author":"David Carr"
+},
+    {
+     "title":"The Thorn Birds"
+      "author":"Colleen McCollough"
+},
+    {
+     "title":"The Shock Doctrine"
+     "author":"Naomi Klein"
+}
+
+]
